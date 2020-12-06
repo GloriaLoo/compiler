b950eb522ea6f592158bf209103c8a098808f837
GloriaLoo <49942947+GloriaLoo@users.noreply.github.com>
2020-12-06 17:25:33 +0800

    Update GrammarAnalyzer.py

--- src/GrammarAnalyzer.py
+++ src/GrammarAnalyzer.py
@@ -1,11 +1,11 @@
 FIRST = {}
 
 FOLLOW = {}
-# Follow的三种情况：
-# S为开始符号，将$加入Follow(S)
+# Follow(A) 的三种情况：
+# A为开始符号，将$加入Follow(A)
 # S->...Ab...: 将b加入Follow(A)
 # S->...AB...: 将First(B)-{ε}加入Follow(A)
-# A->...aB...: 将Follow(A)加入Follow(B)；a可以是终结符或者非终结符？？？
+# B->...aA...: 将Follow(B)加入Follow(A)；a可以是终结符或者非终结符？？？
 
 # 摘自百度百科
 # 集合 Follow (A) 的定义如下：
